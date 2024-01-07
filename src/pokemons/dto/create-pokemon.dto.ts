a46c7c7d52a437e6dbdba752954a992d66da4694
mikoto <avdb@keemail.me>
2024-01-07 19:08:37 +0000

    Merged GetPokemonDto and GetPokemonDetailsDto

--- src/pokemons/dto/create-pokemon.dto.ts
+++ src/pokemons/dto/create-pokemon.dto.ts
@@ -1,1 +1,131 @@
-export class CreatePokemonDto {}
+import { IsNotEmpty } from 'class-validator';
+
+type NameAndUrl = {
+  name: string;
+  url: string;
+};
+
+type Ability = {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+};
+
+type GameIndex = {
+  game_index: number;
+  version: NameAndUrl;
+};
+
+type ItemVersionDetails = {
+  rarity: number;
+  version: NameAndUrl;
+};
+
+type Item = {
+  item: NameAndUrl;
+  version_details: Array<ItemVersionDetails>;
+};
+
+type VersionGroupDetails = {
+  level_learned_at: number;
+  move_learn_method: NameAndUrl;
+  version_group: NameAndUrl;
+};
+
+type Move = {
+  move: NameAndUrl;
+  version_group_details: Array<VersionGroupDetails>;
+};
+
+type PastTypes = {
+  generation: NameAndUrl;
+  types: Kind[];
+};
+
+type SpriteMap = {
+  back_default?: string | null;
+  back_female?: string | null;
+  back_shiny?: string | null;
+  back_shiny_female?: string | null;
+  front_default?: string | null;
+  front_female?: string | null;
+  front_shiny?: string | null;
+  front_shiny_female?: string | null;
+};
+
+type Sprites = {
+  other: Record<string, SpriteMap>;
+  versions: Record<
+    string,
+    Record<string, SpriteMap & { animated?: SpriteMap | null }>
+  >;
+} & SpriteMap;
+
+type Stat = {
+  base_stat: number;
+  effort: number;
+  stat: NameAndUrl;
+};
+
+type Kind = {
+  slot: number;
+  type: NameAndUrl;
+};
+
+export class JsonPokemonDto {
+  @IsNotEmpty()
+  abilities: Array<Ability>;
+
+  @IsNotEmpty()
+  base_experience: number;
+
+  @IsNotEmpty()
+  forms: Array<NameAndUrl>;
+
+  @IsNotEmpty()
+  game_indices: Array<GameIndex>;
+
+  @IsNotEmpty()
+  height: number;
+
+  @IsNotEmpty()
+  held_items: Array<Item>;
+
+  @IsNotEmpty()
+  is_default: boolean;
+
+  @IsNotEmpty()
+  location_area_encounters: string;
+
+  @IsNotEmpty()
+  moves: Array<Move>;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  order: number;
+
+  @IsNotEmpty()
+  past_types: Array<PastTypes>;
+
+  @IsNotEmpty()
+  species: NameAndUrl;
+
+  @IsNotEmpty()
+  sprites: Sprites;
+
+  @IsNotEmpty()
+  stats: Array<Stat>;
+
+  @IsNotEmpty()
+  types: Array<Kind>;
+
+  @IsNotEmpty()
+  weight: number;
+}
+
+export { Ability as JsonAbilityDto };
--- src/pokemons/dto/get-pokemon.ts
+++ src/pokemons/dto/get-pokemon.ts
@@ -5,8 +5,26 @@ class GetPokemonDto {
   types: Array<TypeDto>;
 }
 
+class GetPokemonDetailsDto extends GetPokemonDto {
+  height: number;
+  weight: number;
+  moves: Array<MoveDto>;
+  order: number;
+  species: string;
+  stats: Array<StatDto>;
+  abilities: Array<AbilityDto>;
+  form: string;
+}
+
 class SpriteDto {
   front_default: string;
+  front_female?: string;
+  front_shiny?: string;
+  front_shiny_female?: string;
+  back_default?: string;
+  back_female?: string;
+  back_shiny?: string;
+  back_shiny_female?: string;
 }
 
 class TypeDto {
@@ -14,4 +32,27 @@ class TypeDto {
   slot: number;
 }
 
-export default GetPokemonDto;
+class MoveDto {
+  move: string;
+  version_group_details: Array<MoveDetailsDto>;
+}
+
+class StatDto {
+  stat: string;
+  base_stat: number;
+  effort: number;
+}
+
+class AbilityDto {
+  stat: string;
+  base_stat: number;
+  effort: number;
+}
+
+class MoveDetailsDto {
+  move_learn_method: string;
+  version_group: string;
+  level_learned_at: number;
+}
+
+export { GetPokemonDto, GetPokemonDetailsDto };
